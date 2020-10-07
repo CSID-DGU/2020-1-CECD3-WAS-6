d7a53b6650999f40041728401493432eba7a007d
OhJaeHyeok <62604084+OhJaeHyeok@users.noreply.github.com>
2020-10-07 12:38:22 +0900

    Add files via upload

--- Fault Localizer/Tarantula/src/gcd.py
+++ Fault Localizer/Tarantula/src/gcd.py
@@ -6,10 +6,10 @@ def gcd(a,b):
     if(a==0):
         return b
     if(b==0):
-        return b    #bug    return a
+        return a
     while(b != 0):
         if(a>b):
-            a = a - b
+            a = a - 1   #bug a = a - b
         else:
             b = b - a
     return a
